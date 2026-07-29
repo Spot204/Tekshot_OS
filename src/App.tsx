675562326620863a51ea37b65aa0401b293fe20f
Spot204 <110775732+Spot204@users.noreply.github.com>
2026-07-29 17:42:24 +0700

    first navbar and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 
 import Sidebar from "./layout/Sidebar";
-import Header from "./components/layout/Header"
+import Header from "./components/layout/Header";
 function App() {
   const [activeTab, setActiveTab] = useState("don-hang");
   return (
-
     <div className="d-3">
-       <Header></Header>
-      <div className="d-flex">
+      <div className="d-flex py-4">
+        <Header></Header>
         <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
-
         <div className="flex-grow-1 p-4">
           <p> {activeTab}</p>
         </div>
