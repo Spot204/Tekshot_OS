109c7e9f13048d08bac3598642bfead50114ecc2
Bao-I <tranquocbaodeptrai882004@gmail.com>
2026-07-29 17:31:48 +0700

    push components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 
+import Sidebar from "./layout/Sidebar";
 function App() {
+  const [roles, setRoles] = useState<string[]>([]);
+  const [activeTab, setActiveTab] = useState("don-hang");
+  return (
+    <div className="d-3">
+      <div className="d-flex">
+        <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+
+        <div className="flex-grow-1 p-4">
+          <p> {activeTab}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
