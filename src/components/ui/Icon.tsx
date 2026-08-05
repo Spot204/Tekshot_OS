9dbe53b656dce2f04b3211fd5c1b0e67622ce321
Eternal <et3rnxl24@gmail.gmail.com>
2026-08-05 10:30:54 +0700

    pushing local master to origin master

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -12,15 +12,9 @@ export default function Icon({ name, size, className, onClick }: IconProps) {
   return (
     <i
       className={clsx(`bi bi-${name}`, className)}
-<<<<<<< HEAD
-      style={size ? { fontSize: size } : undefined }
-      aria-hidden="true" 
-      
-=======
       style={size ? { fontSize: size } : undefined}
       aria-hidden="true"
       onClick={onClick}
->>>>>>> 28d311b4d3648ec4ac1e7f7afb450a7f3a5cd754
     />
   );
 }
